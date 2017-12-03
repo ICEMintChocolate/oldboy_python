4a38d5f0a317a3d97094ff228b73f07d836997a2
fucong <fucong10@163.com>
2017-12-03 16:06:03 +0800

    作业

---
+++ one_day_2017_12_02/while循环.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+# User:fucong
+# Time：2017-12-03
+'''
+a.  使用while循环实现输出2-3+4-5+6...+100 的和
+b. 使用 while 循环实现输出 1,2,3,4,5, 7,8,9, 11,12 使用 while 循环实现输出 1-100 内的所有奇数
+
+e. 使用 while 循环实现输出 1-100 内的所有偶数
+'''
+#a. 使用while循环实现输出2-3+4-5+6...+100 的和
+
+
+
+
+
+
+
+#使用 while 循环实现输出 1,2,3,4,5, 7,8,9, 11,12 使用 while 循环实现输出 1-100 内的所有奇数
+#方法1
+# count = 1
+# print(count)
+# while count <=100:
+#     count += 2
+#     print(count)
+#方法2
+# count=1
+# while count <= 100:
+#      if count %2 != 0:
+#          print(count)
+#      count+=1
+
+#使用 while 循环实现输出 1-100 内的所有偶数
+#方法1
+# count = 0
+# while count < 100:
+#     count += 2
+#     print(count)
+#方法2
+# count=1
+# while count <= 100:
+#      if count %2 == 0:
+#          print(count)
+#      count+=1
+
