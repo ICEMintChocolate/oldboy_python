8b9d9fd6edbaed9c9a240c9733ef5d79dde2bb2b
fucong <fucong10@163.com>
2017-12-03 14:35:56 +0800

    作业

---
+++ one_day_2017_12_02/作业7：用户登录.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+# user:fucong
+# Time：2017-12-03
+'''
+写代码
+实现用户输入用户名和密码,当用户名为 seven 且 密码为 123 时,显示登陆成功,否则登陆失败!
+实现用户输入用户名和密码,当用户名为 seven 且 密码为 123 时,显示登陆成功,否则登陆失败,失败时允许重复输入三次
+实现用户输入用户名和密码,当用户名为 seven 或 alex 且 密码为 123 时,显示登陆成功,否则登陆失败,失败时允许重复输入三次
+
+'''
+#实现用户输入用户名和密码,当用户名为 seven 且 密码为 123 时,显示登陆成功,否则登陆失败!
+# user = 'seven'
+# password = '123'
+# user_input = input('请输入用户名：')
+# pass_input = input('请输入密码：')
+# if user_input == user and pass_input == password:
+#     print('登录成功')
+# else:
+#     print('登录失败')
+
+
+#实现用户输入用户名和密码,当用户名为 seven 且 密码为 123 时,显示登陆成功,否则登陆失败,失败时允许重复输入三次
+# user = 'seven'
+# password = '123'
+# #计数器
+# count = 0
+# while count < 3:
+#     user_input = input('请输入用户名：')
+#     pass_input = input('请输入密码：')
+#     if user_input == user and pass_input == password:
+#         print('登录成功')
+#     else:
+#         count +=1
+#         print('登录失败')
+
+#实现用户输入用户名和密码,当用户名为 seven 或 alex 且 密码为 123 时,显示登陆成功,否则登陆失败,失败时允许重复输入三次
+
+userlist = ['seven','alex']
+password = '123'
+#计数器
+count = 0
+while count < 3:
+    user_input = input('请输入用户名：')
+    pass_input = input('请输入密码：')
+    if user_input in userlist and pass_input == password:
+        print('登录成功')
+    else:
+        count +=1
+        print('登录失败')
