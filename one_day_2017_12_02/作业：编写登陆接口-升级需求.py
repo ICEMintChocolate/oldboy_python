36a7aa4332a46ed3c2c7523972591c0dd33637b0
fucong <fucong10@163.com>
2017-12-04 12:29:46 +0800

    作业

--- one_day_2017_12_02/作业：编写登陆接口-升级需求.py
+++ one_day_2017_12_02/作业：编写登陆接口-升级需求.py
@@ -11,56 +11,52 @@
 
 可以支持多个用户登录 (提示，通过列表存多个账户信息)
 用户3次认证失败后，退出程序，再次启动程序尝试登录时，还是锁定状态（提示:需把用户锁定的状态存到文件里）
+
 '''
-#可以支持多个用户登录 (提示，通过列表存多个账户信息)
-# userlist = ['seven','alex','egon']
-# password = '123'
-# #计数器
-# count = 0
-# while count < 3:
-#     user_input = input('请输入用户名：')
-#     pass_input = input('请输入密码：')
-#     if user_input in userlist and pass_input == password:
-#         print('登录成功')
-#     else:
-#         count +=1
-#         print('登录失败')
 user_list = ['seven','alex','egon']
 user_pass = '123'
 count = 0
-while count < 3:
-    f = open('c:\chenggong.txt','r')
-    user_chengong = f.readlines()
-    f.close()
-    f = open('c:\shibai.txt','r')
-    user_shibai = f.readlines()
-    f.close()
-#第一次正常的循环
-    if user_chengong == [] :
-        user_input = input('请输入用户：')
-        user_pass_input = input('请输入密码：')
-        if user_input in user_list  and user_pass_input == user_pass:
-            f = open('c:\chenggong.txt','w')
-            f.write("%s\n"%user_input)
-            f.close()
-            print('登录成功')
-            count += 1
-            continue
-        else:
-            f = open('c:\shibai.txt','w')
-            f.write("%s\n"%user_input)
-            f.close()
-            print('登录失败')
-            count += 1
-            continue
-    if user_chengong != []:
-            print('%s 登录成功'%user_chengong)
-            print('请在 >>> 后开始输入命令，q 为退出')
-            tag = True
-            while tag:
-                cmd_input = input('>>>')
-                if cmd_input == 'q':
-                    print('%s 成功退出'%user_chengong)
-                    tag = False
-
-            break
+#判断是否有用户存储文件
+import pathlib
+chenggong_file = pathlib.Path('c:\chenggong.txt')
+chenggong_file_status = chenggong_file.is_file()
+shibai_file = pathlib.Path('c:\shibai.txt')
+shibai_file_status = shibai_file.is_file()
+if chenggong_file_status == True and shibai_file_status == True:
+    while count < 3:
+        f = open('c:\chenggong.txt','r')
+        user_chengong = f.readlines()
+        f.close()
+        f = open('c:\shibai.txt','r')
+        user_shibai = f.readlines()
+        f.close()
+    #第一次正常的循环
+        if user_chengong == [] :
+            user_input = input('请输入用户：')
+            user_pass_input = input('请输入密码：')
+            if user_input in user_list  and user_pass_input == user_pass:
+                f = open('c:\chenggong.txt','w')
+                f.write('%s\n'%user_input)
+                f.close()
+                print('登录成功')
+                count += 1
+                continue
+            else:
+                f = open('c:\shibai.txt','w')
+                f.write('%s\n'%user_input)
+                f.close()
+                print('登录失败')
+                count += 1
+                continue
+        if user_chengong != [] :
+                print('%s 登录成功'%user_chengong)
+                print('请在 >>> 后开始输入命令，q 为退出')
+                tag = True
+                while tag:
+                    cmd_input = input('>>>')
+                    if cmd_input == 'q':
+                        print('%s 成功退出'%user_chengong)
+                        tag = False
+                break
+else:
+    print('请到 C 盘的根目录创建 "chenggong.txt" 和 "shibai.txt" 再次运行程序')
