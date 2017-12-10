000f507322d6dbfee1edf9b858c66013cc51210f
fucong <fucong10@163.com>
2017-12-10 10:14:10 +0800

    第二天作业

---
+++ day2_2017_12_09/老师文件/5 字典练习题.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# User:fucong
+# Time：2017-12-09
+msg_dic={
+'apple':10,
+'tesla':100000,
+'mac':3000,
+'lenovo':30000,
+'chicken':10,
+}
+user_input = input("请输入要购买的物品名称：")
