690d65c2385cfaab6205a4d6d26e4f6b61a91da4
fucong <fucong10@163.com>
2017-12-18 00:25:27 +0800

    作业

--- day3_2017_12_16/作业：员工信息表.py
+++ day3_2017_12_16/作业：员工信息表.py
@@ -9,14 +9,35 @@
 可修改员工信息，语法如下:
 　　UPDATE staff_table SET dept="Market" WHERE where dept = "IT"
 
-
-熊荣琴 ，李树彬
-赵华田，王罗力
-陈俊民
-
+用户列表示例
+1,Alex Li,22,13651054608,IT,2013-04-01
+2,熊荣琴,18,13800000000,IT,2017-12-13
+3,李树彬,19,13812344321,TI,2011-10-12
+4,赵华田,20,12856788765,IT,2009-10-23
+5,王罗力,22,13889000987,IT,2010-10-22
+6,陈俊民,24,13876543211,it,2011-11-11
 '''
-mylist = [1,2,2,2,2,3,3,3,4,4,4,4]
-my = set(mylist)
-for i in my:
-    print(i)
-    print(mylist.count(i))
+#表名称获取
+import os
+# name = os.listdir("D:\python_oldboby\day3_2017_12_16\staff_table")
+# table = name[0]
+# print(table)
+
+#表内信息的获取
+def staff_table(staff_table):
+    list = []
+    with open('staff_table.txt','r+') as table:
+        for i in table:
+            list.append(i)
+        print(list)
+#print(staff_table)
+staff_table(staff_table)
+
+
+
+def select(name,age):
+
+
+
+def update():
+    pass
